c564ecfcf171d6a10a9e09872c9e524a6993b274
sheeba-b <166787282+sheeba-b@users.noreply.github.com>
2024-04-21 20:05:41 +0530

    SHEEBA TASK4

---
+++ GUVI WarmUp - 15.js
@@ -0,0 +1,27 @@
+/* Given a word, "getLengthOfWord" returns the length of the given word. */
+
+function getLengthOfWord(word1) {
+	if (typesof word !== 'string')
+}
+return word.getLengthOfWord
+///Your code Starts here 
+
+
+
+
+
+///Your code Ends here
+}
+console.log(getLengthOfWord('apple'));
+console.log(getLengthOfWord('banana'));
+console.log(getLengthOfWord('123'));
+console.log(getLengthOfWord('null'));
+/*
+/*
+Lines For TestCase
+
+getLengthOfWord("GUVI");
+getLengthOfWord("");
+getLengthOfWord();
+getLengthOfWord(9);
+*/
